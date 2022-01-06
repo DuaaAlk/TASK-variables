1344531b87c1892a16468217762a139c3b3cfb64
DuaaAlk <engduaa2013@gmail.com>
2022-01-06 18:26:08 +0300

    I am done ;P

--- index.js
+++ index.js
@@ -11,10 +11,11 @@
 
 // Part 1 answer 👇🏻 ...
 
-
-
-
-
+let fullName = "Duaa Abdullah Alkandari";
+let yearOfBirth = "1990"
+let hobby = "Traveling"
+let funFact = "Eating Chocolate toooo much";
+const image = "https://img-19.ccm2.net/cI8qqj-finfDcmx6jMK6Vr-krEw=/1500x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg"
 
 /**
  * Part 2: String Interpolation
@@ -29,11 +30,9 @@
  * You can uncomment a line by removing the // at the start
  */
 
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
-
-
+const fullNameString = `My name is ${fullName}`;
+const yearOfBirthString = `I am ${2021-yearOfBirth}`;
+const hobbyString = `My hobby is ${hobby}`
 
 
 /**
@@ -45,14 +44,18 @@ let stars = 0;
 
 function incrementBy1() {
   // Increment stars by 1 👇🏻
+  stars++;
 }
 function decrementBy1() {
   // decrement stars by 1 👇🏻
+  stars--;
 }
 
 function incrementBy2() {
   // Increment stars by 2 👇🏻
+  stars+=2;
 }
 function decrementBy2() {
   // decrement stars by 2 👇🏻
+  stars-=2;
 }
